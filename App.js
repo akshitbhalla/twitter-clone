644d96a3c09b65ef78f86885455b8072a6d435a4
Akshit Bhalla <akshitbhalla13@gmail.com>
2017-12-06 23:10:42 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { AppRegistry, View, Text, StyleSheet } from 'react-native'
+
+class Counter extends Component {
+
+  state = {count: 0}
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count + 1})
+    }, 1000)
+  }
+
+  render() {
+    const {count} = this.state
+    const {color, size} = this.props
+
+    return (
+      <Text style={{color, fontSize: size}}>
+        {count}
+      </Text>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Counter color={'lightblue'} size={16} />
+        <Counter color={'skyblue'} size={32} />
+        <Counter color={'steelblue'} size={80} />
+        <Counter color={'darkblue'} size={140} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+AppRegistry.registerComponent('App', () => App)
